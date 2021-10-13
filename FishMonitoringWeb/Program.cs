91f544554ca0d0946ec6435c049b50ec35b854d9
User <User@WINDEV2108EVAL>
2021-10-12 23:32:37 -0700

    Add project files.

---
+++ FishMonitoringWeb/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FishMonitoringConsole;
+using System.IO;
+
+namespace FishMonitoringWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/home/name/form.html";
+            //string path = @"C:\Users\gutuf\Desktop\FishMonitoring-develop\FishMonitoringWeb\form.html";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine("Content-Type: text/html \n\n");
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
